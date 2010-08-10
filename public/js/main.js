13374cdfaf10df42dc22049c68102c2dd7b75fe4
Grayson Manley <g.manley@tukaiz.com>
2010-08-10 18:51:17 -0500

    Couple more tweaks to the lightbox.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
-$('#content').infinitescroll({
-  navSelector  : "ul.pager",
-  nextSelector : "ul.pager li.next.jump a",
-  itemSelector : "#content #thumbnails",
-  local      : true
-});
+//$('#content').infinitescroll({
+//  navSelector  : "ul.pager",
+//  nextSelector : "ul.pager li.next.jump a",
+//  itemSelector : "#content #thumbnails",
+//  local      : true
+//});
 
 $(document).ready(function(){
 
+$("a[rel='gallery1']").colorbox({maxWidth:"95%", maxHeight:"95%"});
 
-$("a[rel='gallery1']").live("click", function(){ 
-  $(this).colorbox({open:true, maxWidth:"95%", maxHeight:"95%"}); 
-  return false;
-});
+//$("a[rel='gallery1']").live("click", function(){ 
+//  $(this).colorbox({open:true, maxWidth:"95%", maxHeight:"95%"}); 
+//  return false;
+//});
 
 $('ul.pager:first').hide();
 
