8cf720fea17ee2a83f0b3e6ccbb46f64f686261e
Grayson Manley <g.manley@tukaiz.com>
2010-08-10 19:02:56 -0500

    More tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,5 @@
-//$('#content').infinitescroll({
-//  navSelector  : "ul.pager",
-//  nextSelector : "ul.pager li.next.jump a",
-//  itemSelector : "#content #thumbnails",
-//  local      : true
-//});
-
 $(document).ready(function(){
 
 $("a[rel='gallery1']").colorbox({maxWidth:"95%", maxHeight:"95%"});
-
-//$("a[rel='gallery1']").live("click", function(){ 
-//  $(this).colorbox({open:true, maxWidth:"95%", maxHeight:"95%"}); 
-//  return false;
-//});
-
-$('ul.pager:first').hide();
 
 });
