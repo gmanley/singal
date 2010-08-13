88cdb378b58ed4182892c1455988fe0fbd43fd8d
Grayson Manley <g.manley@tukaiz.com>
2010-08-13 01:34:27 -0500

    Fixed an issue with my regex

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $("a[rel='gallery1']").colorbox({maxWidth:"95%", maxHeight:"95%"});
 
 $("a.gallery['href']").each(function() { 
-  this.href = this.href.replace(/(http|https)(:\/\/)(www\,|\w+\.)?(\d|\w+)(:\d+)?(\/bot\?)/, "");
+  this.href = this.href.replace(/(http|https)(:\/\/)(www\.|.+\.)?(\d|\w+)(:\d+)?(\/bot\?)/, "");
 });
 
 });
