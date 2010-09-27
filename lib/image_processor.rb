cd67741346eff352f8f8543d50690ca4a703d014
Gray Manley <gray.manley@gmail.com>
2010-09-27 11:09:08 -0500

    Added the image processor class.

--- lib/image_processor.rb
+++ lib/image_processor.rb
@@ -2,60 +2,83 @@ class ImageProcessor
 
   def initialize
     @config = File.open(Pathname(Sinatra::Application.root)/"config/picasa.yml") { |file| YAML.load(file) }
-    picasa = Picasa.new
-    picasa.login(@config['credentials']['email'], @config['credentials']['password'])
+    @picasa = Picasa.new
+    @picasa.login(@config['credentials']['email'], @config['credentials']['password'])
+    run
+  end
+
+  def cache?
+    @config['options']['cache']
   end
 
   def run
-    collect_albums
+    find_albums
     process_albums
-    cache_images
+    process_images
   end
 
-  def collect_albums
+  def find_albums
     @albums = []
 
-    albums = Nokogiri::XML(open("http://picasaweb.google.com/data/feed/api/user/#{picasa.user_id}?kind=album&access=private&fields=entry(gphoto:id)", "Authorization" => "GoogleLogin auth=#{picasa.auth_key}", 'GData-Version' => '2'))
+    albums = Nokogiri::XML(open("http://picasaweb.google.com/data/feed/api/user/#{@picasa.user_id}?kind=album&fields=entry(gphoto:id)", "Authorization" => "GoogleLogin auth=#{@picasa.auth_key}", 'GData-Version' => '2'))
     albums.xpath("//xmlns:entry//gphoto:id").each do |a|
       @albums << a.content
     end
-    @albums
   end
 
-  def process_images
+  def process_albums
     @images = []
 
     @albums.each do |album_id|
-      doc = Nokogiri::XML(open("http://picasaweb.google.com/data/feed/api/user/#{picasa.user_id}/albumid/#{album_id}?kind=photo&thumbsize=#{@config['options']['thumb_size']}&imgmax=#{@config['options']['max_size']}&fields=entry(media:group(media:content,media:thumbnail))", "Authorization" => "GoogleLogin auth=#{picasa.auth_key}", 'GData-Version' => '2'))
+      doc = Nokogiri::XML(open("http://picasaweb.google.com/data/feed/api/user/#{@picasa.user_id}/albumid/#{album_id}?kind=photo&thumbsize=#{@config['options']['thumb_size']}&imgmax=#{@config['options']['max_size']}&fields=entry(media:group(media:content,media:thumbnail))", "Authorization" => "GoogleLogin auth=#{@picasa.auth_key}", 'GData-Version' => '2'))
       doc.remove_namespaces!
 
       doc.xpath("//entry").each do |entry|
         entry.children.each do |n|
           if n.node_name == "group"
-
-            image = Hash.new
-
-            n.children.each do |g|
-              case g.node_name
-              when "content"
-                image["image"] = g.attribute("url").content
-              when "thumbnail"
-                image["thumb"] = g.attribute("url").content
-              end
-            end
-
-            @images << image
+            @image_group = []
+            @image_group << n
           end
         end
       end
     end
   end
 
-  def cache_images
+  def process_images
+    image = Hash.new
+    @image_group.each do |image_group|
+      image_group.children.each do |g|
+        case g.node_name
+        when "content"
+          image["image"] = g.attribute("url").content
+        when "thumbnail"
+          image["thumb"] = g.attribute("url").content
+        end
+      end
+    end
+    @images << image
+  end
+
+
+  def cache_image_urls
     @images.each do |image|
       as = Photo.new
       as.attributes = image
       as.save
+    end
+  end
+end
+
+class AlbumBuilder < ImageProcessor
+end
+
+class CacheBuilder < ImageProcessor
+  def run
+    find_albums
+    process_albums
+    process_images
+    if cache?
+      cache_image_urls
     end
   end
 end
