e1e0d0b346f5351e4784c2057b1c81e47e3dd0b1
Gray Manley <gray.manley@gmail.com>
2010-12-23 12:11:49 -0600

    Some changes to the page layout.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("a[rel='gallery1']").colorbox({maxWidth:"95%", maxHeight:"95%"});
+  // Initialize colorbox plugin.
+  $(".thumbnail").colorbox({
+    maxWidth: "95%",
+    maxHeight: "95%"
+  });
 
-$("a.gallery['href']").each(function() { 
-  this.href = this.href.replace(/(http|https)(:\/\/)(www\.|.+\.)?(\d|\w+)(:\d+)?(\/bot\?)/, "");
-});
+  function fitImages () {
+
+    // Reset image size
+    $("#thumbnails").css({'width': "100%"});
+
+    var colWrap = $("#thumbnails").width();
+    // Number of 160px images that can fit per row, rounded down to a whole number.
+    var colNum = Math.floor(colWrap / 160);
+    // Width of row divided by the ammount of columns it can fit, rounded down to a whole number.
+    var colFixed = Math.floor(colWrap / colNum);
+
+    // Set width of row in pixels instead of percent (Fixes cross browser issue).
+    $("#thumbnails").css({'width': colWrap});
+    // Same as above but for re-adjusted column.
+    $(".thumbnail img").css({'width': colFixed});
+  }
+
+  // Run on page load...
+  fitImages();
+  // and whenever the window is resized.
+  $(window).resize(function () {
+    fitImages();
+  });
 
 });
