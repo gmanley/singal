6fcb91ca749849daf4911788912bf02b125ef17a
Gray Manley <gray.manley@gmail.com>
2011-01-16 19:40:59 -0600

    Clean up styling after switch to mongo.

--- lib/pagination_helper.rb
+++ lib/pagination_helper.rb
@@ -3,6 +3,31 @@ require 'will_paginate/view_helpers/link_renderer'
 
 WillPaginate::ViewHelpers::LinkRenderer.class_eval do
   protected
+
+  def page_number(page)
+    unless page == current_page
+      tag(:li, link(page, page, :rel => rel_value(page)))
+    else
+      tag(:li, tag(:p, page), :class => 'active')
+    end
+  end
+
+  def gap
+    '<li class="gap">&hellip;</li>'
+  end
+
+  def previous_or_next_page(page, text, classname)
+    if page
+      tag(:li, link(text, page), :class => classname)
+    else
+     tag(:li, text, :class => classname + ' disabled')
+    end
+  end
+
+  def html_container(html)
+    tag(:ul, html, container_attributes)
+  end
+
   def url(page)
     url = @template.request.url
     if page == 1
